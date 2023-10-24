c2c30b263353efbfd8ee1b1d949974b6bfaef2e7
sarvan2904 <147610712+sarvan2904@users.noreply.github.com>
2023-10-24 12:18:41 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
-basic.forever(function () {
-	
-})
+/**
+ * Blocks for driving Motor connected to ports M1, M2 */
+//% weight=100 color=#FFA500 icon="\uf21c" block="Motor Driver"
+namespace quadrobotics_motor_driver {
+    /************************************************************************************************************************************************
+    * micro:bit motor driver blocks 
+    ************************************************************************************************************************************************/
+    /*Note that Forward and reverse are slightly arbitrary, as it depends on how the motor is wired...*/
+    export enum MotorDirection {
+        //% block="Clockwise ⟳"
+        Clockwise,
+        //% block="Anti-Clockwise ⟲"
+        Anti-Clockwise
+    }
+
